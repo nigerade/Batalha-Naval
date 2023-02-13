a269d6a94a2aa1992cc5243a0f0a0e50b9dce181
Nicholas Gerade <nicholasgerade@gmail.com>
2023-02-13 17:46:19 -0300

    Comentando o Código

--- main.c
+++ main.c
@@ -34,34 +34,36 @@ int main() // COMEÇO PROGRAMA
     // FIM DECLARANDO VARIÁVEIS
 
 	
-	//preenchendo as matrizes com ÁGUA
-	for(l=0;l<10;l++)
+	// PREENCHENDO AS MATRIZES COM ÁGUA
+	for(l=0; l<10; l++)
 	{	
-        for(c=0;c<10;c++)
+        for(c=0; c<10; c++)
 		{	
             tabuleiroHumano[l][c] = AGUA;
 			tabuleiroComputador[l][c] = AGUA;
 		}
 	}
+    // FIM PREENCHENDO AS MATRIZES COM ÁGUA
+
 	
-	//entrada dos navios no tabuleiro do humano
+	// ENTRADA DOS NAVIOS NO TABULEIRO DO HUMANO
 	for(i=1; i<=6; i++) //Sao 6 navios
 	{	
 	    do{
-	        printf("Entre com as coordenadas de linha e coluna do %d o. navio:\n", i);
+	        printf("Entre com as coordenadas de Linha e Coluna do %d o. Navio:\n", i);
 		    scanf("%d%d", &l, &c);
 		    if(tabuleiroHumano[l][c] == NAVIO || tabuleiroHumano[l][c+1] == NAVIO || tabuleiroHumano[l][c+2] == NAVIO)
-		        printf("Erro! Já existe navio aí....\n");
+		        printf("Erro! Já existe navio nesta coordenada....\n");
 	    } while(tabuleiroHumano[l][c] == NAVIO || tabuleiroHumano[l][c+1] == NAVIO || tabuleiroHumano[l][c+2] == NAVIO);
 		
 		tabuleiroHumano[l][c] = NAVIO;   //o navio e formado por 3 partes
 		tabuleiroHumano[l][c+1] = NAVIO;
 		tabuleiroHumano[l][c+2] = NAVIO;
-		
-		
-
      }
-	//sorteio dos navios no tabuleiro do computador
+    // FIM ENTRADA DOS NAVIOS NO TABULEIRO DO HUMANO
+
+
+	// SORTEIO DOS NAVIOS NO TABULEIRO DO COMPUTADOR
 	for(i=1; i<=6; i++) //Sao 6 navios
 	{	
         do{
@@ -69,13 +71,14 @@ int main() // COMEÇO PROGRAMA
 		    c = rand()%8;
 	    } while(tabuleiroComputador[l][c] == NAVIO || tabuleiroComputador[l][c+1] == NAVIO || tabuleiroComputador[l][c+2] == NAVIO);
 		
-		tabuleiroComputador[l][c] = NAVIO;   //o navio e formado por 3 partes
-		tabuleiroComputador[l][c+1] = NAVIO;
-		tabuleiroComputador[l][c+2] = NAVIO;
-		
-	
+		tabuleiroComputador[l][c] = NAVIO; // o navio e formado por 3 partes
+		tabuleiroComputador[l][c+1] = NAVIO; // coluna +1 
+		tabuleiroComputador[l][c+2] = NAVIO; // coluna +2
 	}
+    // FIM SORTEIO DOS NAVIOS NO TABULEIRO DO COMPUTADOR
 
+
+    // LOOP QUE FAZ O JOGO CONTINUAR ENQUANTO NÃO ATINGIR 18 PONTOS (TODOS NAVIOS)
 	while(pontosHumano<18 && pontosComputador<18)
 	{	//Tiro do Humano
 		do
@@ -101,8 +104,6 @@ int main() // COMEÇO PROGRAMA
 		    tabuleiroComputador[l][c] = TIRONAVIO;   
 		}
 	
-		
-
 		//Tiro do Computador
 		if (modoComputador ==  ALEATORIO)
 		{ 
@@ -114,11 +115,10 @@ int main() // COMEÇO PROGRAMA
 
 	   else // modointeligente
 	   {
-	       l = ultimoL;
+	      l = ultimoL;
 	      if (sentido == DIREITA)  c = ultimoC+1;
 	      if (sentido == ESQUERDA) c = ultimoC-1;
 	   }
-		
 		
 		printf("Eu estou atirando nas coordenadas %d, %d\n", l, c);
 	    if(tabuleiroHumano[l][c] == AGUA)
@@ -143,7 +143,8 @@ int main() // COMEÇO PROGRAMA
 		    modoComputador = INTELIGENTE;
 
 		    if (contTiros == 3)
-		    {   modoComputador = ALEATORIO;
+		    {   
+                modoComputador = ALEATORIO;
 		        contTiros = 0;
 		        sentido = DIREITA;
 		    }
@@ -151,40 +152,51 @@ int main() // COMEÇO PROGRAMA
 		
 		printf("\nTabuleiro do Humano:\n");
 		printf("    0 1 2 3 4 5 6 7 8 9\n\n");
-		for(l=0; l < 10; l++)
+		for(l=0; l < 10; l++) // LIMITA A 10 POSIÇÕES (10x10) - LINHA
 		{	
             printf("%d  |", l);
-			for(c=0; c < 10; c++)
+			for(c=0; c < 10; c++) // LIMITA A 10 POSIÇÕES (10x10) - COLUNA
 			{	
                 if(tabuleiroHumano[l][c]==TIRONAGUA)
-					printf("A|");
-				else if(tabuleiroHumano[l][c]==TIRONAVIO)
-					printf("X|");
-				else printf(" |");
+					printf("A|"); // ACERTO
+
+				else if(tabuleiroHumano[l][c]==TIRONAVIO) 
+					printf("X|"); // ERRO
+
+				else printf(" |"); // PAREDE
 			}
+
 			printf("\n");
 		
 		}
 		
 		printf("\nTabuleiro do Computador:\n");
 		printf("    0 1 2 3 4 5 6 7 8 9\n\n");
-		for(l=0; l < 10; l++)
+		for(l=0; l < 10; l++) // LIMITA A 10 POSIÇÕES (10x10) - LINHA
 		{	
             printf("%d  |", l);
-			for(c=0; c < 10; c++)
+			for(c=0; c < 10; c++) // LIMITA A 10 POSIÇÕES (10x10) - COLUNA
 			{	
                 if(tabuleiroComputador[l][c]==TIRONAGUA)
-					printf("A|");
+					printf("A|"); // ERRO
+
 				else if(tabuleiroComputador[l][c]==TIRONAVIO)
-					printf("X|");
-				else printf(" |");
+					printf("X|"); // ACERTO
+
+				else printf(" |"); // PAREDE
 			}
+
 			printf("\n");
 		}
 	}
+    // FIM LOOP QUE FAZ O JOGO CONTINUAR ENQUANTO NÃO ATINGIR 18 PONTOS (TODOS NAVIOS)
 
+
+    // CONDICIONAL (SE ATINGIR 18 PONTOS, O JOGO ACABA)
 	if(pontosHumano == 18) printf("Você ganhou!!!!\n");
 	else printf("Eu ganhei, patinho!!!!\n");
+    // FIM CONDICIONAL (SE ATINGIR 18 PONTOS, O JOGO ACABA)     
+
 
 	return 0;
 	}
